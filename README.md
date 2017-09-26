747cdf3a4d2a5f5d505558fc704771f184d29f21
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-09-26 16:58:29 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SiriConversation
+# SpeakingApp
---
+++ SiriConversation/Word.swift
@@ -0,0 +1,19 @@
+//
+//  Word.swift
+//  SiriConversation
+//
+//  Created by SuzukiShigeru on 2017/09/26.
+//  Copyright © 2017年 Shigeru Suzuki. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Word: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
